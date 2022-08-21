96676ec0f8590b0ec36ed54a8bbdaca676e981ac
Jelly Han <jellyhan@Jellys-MacBook-Pro.local>
2022-08-21 11:40:07 -0700

    lc 76 unique window chars added

---
+++ string/lc76_minWindow_uqniueChar.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/minimum-window-substring/discuss/2460454/JavaScript-object-literal-and-counting-chars
+
+var minWindow = function (s, t) {
+  const tmap = {};
+  [...t].forEach((el) => {
+    tmap[el] = (tmap[el] || 0) + 1;
+  });
+
+  let res = "";
+  let matched = 0;
+  let start = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (tmap[s[i]] > 0) {
+      matched++;
+    }
+    tmap[s[i]]--;
+    while (matched === t.length && start <= i) {
+      // start === i is the limit
+      if (res === "" || i - start + 1 < res.length) {
+        res = s.slice(start, i + 1);
+      }
+      if (tmap[s[start]] !== undefined) {
+        if (++tmap[s[start]] > 0) {
+          matched--;
+        }
+      }
+      start++;
+    } // end while
+  } // end for
+  return res;
+};
